da915f073efcb28a46a96221873cd7ee6b04d2b4
karlind <karlind@users.noreply.github.com>
2019-03-27 21:08:40 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name='ndspeed',
+    version='0.1',
+    author='karlind',
+    author_email='karlind@users.noreply.github.com',
+    description='A python package to display network and disk speed',
+    long_description=long_description,
+    url='https://github.com/karlind/ndspeed',
+    license='GPL2.0',
+    packages=find_packages(),
+    install_requires = [
+        'psutil',
+        'terminaltables',
+    ]
+)
