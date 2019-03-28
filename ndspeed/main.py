49ca5ae4a338be15e43914c1f01866f22a639bf0
karlind <karlind@users.noreply.github.com>
2019-03-28 14:04:08 +0800

    add command-line args to main.py

--- ndspeed/main.py
+++ ndspeed/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 
 
 app_home_dir = os.path.dirname(os.path.realpath(__file__))
@@ -10,10 +11,19 @@ from meters.net_meter import NetMeter
 from scheduler import Scheduler
 
 
-def main():
-    scheduler = Scheduler(interval=1)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--interval', help='interval second to flush', default=1.0, type=float)
+    parser.add_argument('-c', '--card', action='append', help='network card names to display', choices=NetMeter.get_all_nic())
+    args = parser.parse_args()
+    return args
 
-    net_meter = NetMeter()
+
+def main():
+    args = parse_args()
+    scheduler = Scheduler(interval=args.interval)
+
+    net_meter = NetMeter(nic_list=args.card if args.card else [])
     scheduler.add_meter(net_meter)
 
     scheduler.loop()
